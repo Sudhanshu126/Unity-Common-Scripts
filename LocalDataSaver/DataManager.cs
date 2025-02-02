a6d00a0a2139ad4f3e0083770d2aca6937edec98
Sudhanshu Kr. Suman <72366741+Sudhanshu126@users.noreply.github.com>
2025-02-02 08:28:40 +0530

    Create DataManager.cs

---
+++ LocalDataSaver/DataManager.cs
@@ -0,0 +1,49 @@
+using System.IO;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    //Properties
+    public static DataManager Instance {  get; private set; }
+    public PlayerData _PlayerData { get; private set; }
+
+    //---MonoBehaviour methods---
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        LoadPlayerData();
+    }
+
+    //---Class methods---
+    //Saves player data
+    public void SavePlayerData()
+    {
+        string data = JsonUtility.ToJson(_PlayerData);
+        string path = Application.persistentDataPath + "/PlayerData.json";
+
+        File.WriteAllText(path, data);
+    }
+
+    //Loads game data
+    public void LoadPlayerData()
+    {
+        string path = Application.persistentDataPath + "/GameData.json";
+
+        if (File.Exists(path))
+        {
+            string data = File.ReadAllText(path);
+            _PlayerData = JsonUtility.FromJson<PlayerData>(data);
+        }
+        else
+        {
+            _PlayerData = new PlayerData();
+        }
+    }
